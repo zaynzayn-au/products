90a88a26ab496a5b1c1da7f06da0915e74115488
Zayn Hwang <yz.huang1218@gmail.com>
2020-04-04 14:08:56 +1100

    excel输入的全新版本

--- product.py
+++ product.py
@@ -11,3 +11,10 @@ print(products)
 
 for p in products:
 	print(p[0],'的价格是', p[1])
+
+#with是python独有功能，自动close已经打开的文档
+with open('products.csv','w', encoding = 'utf-8') as f:
+	f.write('名字,价格\n')#加入excel title 乱码要用excel data 导入
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
+
