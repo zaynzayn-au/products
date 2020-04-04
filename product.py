af41834ada2ff3e34075a6189221f3e5f977f0b9
Zayn Hwang <yz.huang1218@gmail.com>
2020-04-04 18:48:42 +1100

    新增查询文件是否存在功能

--- product.py
+++ product.py
@@ -1,4 +1,21 @@
+import os # operating system
+
+#读取文件
 products = []
+if os.path.isfile('products.csv'):#检查文件是否存在
+	print('已找到文件')
+	with open('products.csv','r', encoding = 'utf-8') as f:
+		for line in f:
+			if '名字,价格' in line:
+				continue #继续loop
+			name, price = line.strip().split(',')
+			products.append([name,price])
+	print(products)
+
+
+else:
+	print('文件不存在')
+
 with open('products.csv','r', encoding = 'utf-8') as f:
 	for line in f:
 		if '名字,价格' in line:
@@ -7,7 +24,7 @@ with open('products.csv','r', encoding = 'utf-8') as f:
 		products.append([name,price])
 print(products)
 
-
+#使用者输入商品名字和价格
 while True:
 	name = input('请输入商品名称：')
 	if name == 'q':
@@ -17,6 +34,7 @@ while True:
 	products.append([name , price])
 print(products)
 
+#印出购买记录
 for p in products:
 	print(p[0],'的价格是', p[1])
 
